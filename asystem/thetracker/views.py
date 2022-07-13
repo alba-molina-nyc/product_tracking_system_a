df2ddc77f345ba4c6383941cd3ecf748a387bdb4
Username <albavmolinanyc@gmail.com>
2022-07-13 15:21:29 -0400

    successfully moved memo filter over to its own page

--- asystem/thetracker/views.py
+++ asystem/thetracker/views.py
@@ -8,12 +8,12 @@ from django.contrib.auth.mixins import PermissionRequiredMixin
 class HomeView(ListView):
     model = Job
     filter = JobFilter
-    
     template_name = 'home.html'
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context['filter'] = JobFilter(self.request.GET, queryset=self.get_queryset())
-        return context
+
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['filter'] = JobFilter(self.request.GET, queryset=self.get_queryset())
+    #     return context
     
 
 class JobDetailView(DetailView):
@@ -34,8 +34,29 @@ class AddMemoView(PermissionRequiredMixin, CreateView):
     model = Memo
     template_name = 'add_memo.html'
     fields = '__all__'
+    # def get_context_data(self, **kwargs):
+    #     context = super().get_context_data(**kwargs)
+    #     context['filter'] = MemoFilter(self.request.GET, queryset=self.get_queryset())
+    #     return context
+
+
+class FilterByJobView(ListView):
+    model = Job
+    filter = JobFilter
+    template_name = 'jfilter.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['filter'] = JobFilter(self.request.GET, queryset=self.get_queryset())
+        return context
+
+
+class FilterByMemoView(ListView):
+    model = Memo
+    filter = MemoFilter
+    template_name = 'mfilter.html'
+    fields = '__all__'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['filter'] = MemoFilter(self.request.GET, queryset=self.get_queryset())
         return context
-    
