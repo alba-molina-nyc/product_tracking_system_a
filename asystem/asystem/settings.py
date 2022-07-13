8eb8fb8c117f5e398308909b3f13949b4b92050e
Username <albavmolinanyc@gmail.com>
2022-07-13 09:38:46 -0400

    set up with psql db

--- asystem/asystem/settings.py
+++ asystem/asystem/settings.py
@@ -77,20 +77,23 @@ WSGI_APPLICATION = 'asystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'thetracker',
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+
+
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'asystem',
+    }
+}
 
 
 
